c7fbb24002429ece63c790d6b8f463c5fbbe65dc
Angelo Cigna <acigna1982@gmail.com>
2017-11-30 23:54:42 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = [
+  `snickers`,
+  `hundred grand`,
+  `kitkat`,
+  `skittles`
+]
+
+function addElementToBeginningOfArray(array, element) {
+  var check = [`foo`, 1]
+  // chocolateBars[0] = `foo`
+  // chocolateBars[1] = 1
+  // return chocolateBars
+  return check
+}
+
+  //const moreChocolate = [`hershey`, ...chocolateBars]
+
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
